e956989759f66b9bd92a0c04fa74e9a402f9e89e
Githongo <jeffreygithongo@gmail.com>
2020-07-14 01:03:39 +0300

    Adds current user phone as value in transaction mpesa phone input field

--- resources/views/pages/transact.blade.php
+++ resources/views/pages/transact.blade.php
@@ -34,7 +34,7 @@
                             <form method="POST" action="/transactions">
                                 @csrf
                                 <center><img src="{{ asset('assets/img/mpesa.png') }}" alt="mpesa-logo"></img></center>
-                                <div class="form-group"><label class="small mb-1" for="inputPhone">Mpesa Phone</label><input class="form-control" id="inputPhone" type="text" placeholder="e.g. 254722000000" name="phone" required/></div>
+                                <div class="form-group"><label class="small mb-1" for="inputPhone">Mpesa Phone</label><input class="form-control" id="inputPhone" type="text" placeholder="e.g. 254722000000" name="phone" value="{{ Auth::user()->phone }}" required/></div>
             
                                 <div class="form-row">
                                     <div class="col-md-5">
